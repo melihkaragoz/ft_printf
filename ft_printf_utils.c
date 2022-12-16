39329657a754fba494498f0472b02db2d915903f
melihkaragoz <melihkkaragoz@hotmail.com>
2022-12-16 22:16:25 +0300

    some functions fixed

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: mkaragoz <mkaragoz@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 10:09:02 by mkaragoz          #+#    #+#             */
-/*   Updated: 2022/12/16 22:10:41 by mkaragoz         ###   ########.fr       */
+/*   Updated: 2022/12/16 22:15:14 by mkaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,29 +22,6 @@ int	ft_putstr(char *str)
 	while (str[i])
 		write(1,str+(i++),1);
 	return (len);
-}
-
-
-int	ft_print_hex(unsigned long nb, char x)
-{
-	if (nb >= 16)
-	{
-		ft_print_hex(nb / 16,'x');
-		ft_print_hex(nb % 16,'x');
-	}
-	else if (nb < 16 && nb >= 10)
-	{
-		char *hex = "abcdef";
-		char c;
-		c = hex[nb % 10];
-		if (x == 'X')
-			ft_putchar(ft_toupper(c));
-		else if (x == 'x')
-			ft_putchar(c);
-	}
-	else
-		ft_putchar(nb + 48);
-	return (ft_strlen(ft_itoa(nb)));
 }
 
 void	ft_putchar(char c)
@@ -65,6 +42,19 @@ int	ft_putnbr(int nb)
 	if (nb < 10)
 		ft_putchar(nb + 48);
 	return (ft_strlen(ft_itoa(nb)));
+}
+
+int ft_print_hex(unsigned long nb, char x)
+{
+	(void)x;
+	int tmp = 0;
+	if (nb >= 16)
+		tmp += ft_printx(nb / 16,x);
+	if (x == 'x')
+		tmp += write(1, &"0123456789abcdef"[nb % 16], 1);
+	else if (x == 'X')
+		tmp += write(1, &"0123456789ABCDEF"[nb % 16], 1);
+	return tmp;
 }
 
 int	check_print_type(char *str, va_list va, int i)
@@ -89,17 +79,4 @@ int	check_print_type(char *str, va_list va, int i)
 		len += ft_printx(va_arg(va,unsigned long),'x');
 	}
 	return (len);
-}
-
-int ft_printx(unsigned long nb, char x)
-{
-	(void)x;
-	int tmp = 0;
-	if (nb >= 16)
-		tmp += ft_printx(nb / 16,x);
-	if (x == 'x')
-		tmp += write(1, &"0123456789abcdef"[nb % 16], 1);
-	else if (x == 'X')
-		tmp += write(1, &"0123456789ABCDEF"[nb % 16], 1);
-	return tmp;
 }
