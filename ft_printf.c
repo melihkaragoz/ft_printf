5b45542ff75cb5e6378afda4969c4a768c7c643b
melihkaragoz <melihkkaragoz@hotmail.com>
2022-12-16 17:52:54 +0300

    i have no idea what have i done

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mkaragoz <mkaragoz@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 20:44:48 by mkaragoz          #+#    #+#             */
-/*   Updated: 2022/12/16 13:10:05 by mkaragoz         ###   ########.fr       */
+/*   Updated: 2022/12/16 13:16:54 by mkaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 int	ft_printf(const char *str, ...)
 {
-	int	pr_len;
-	int	i;
-	va_list va;
+	int		pr_len;
+	int		i;
+	va_list	va;
 
 	i = 0;
 	pr_len = 0;
-	va_start(va,str);
+	va_start(va, str);
 	while (str[i])
 	{
 		if (str[i] != '%')
 		{
-			write(1,str+(i++),1);
+			write(1, str + (i++), 1);
 			pr_len++;
 		}
- 		else if(str[i] == '%')
+		else if (str[i] == '%')
 		{
 			i++;
-			pr_len += check_print_type((char *)str,va,i++);
+			pr_len += check_print_type((char *)str, va, i++);
 		}
 	}
 	va_end(va);
--- ft_printf.h
+++ ft_printf.h
@@ -6,18 +6,23 @@
 /*   By: mkaragoz <mkaragoz@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 20:46:45 by mkaragoz          #+#    #+#             */
-/*   Updated: 2022/12/16 13:06:58 by mkaragoz         ###   ########.fr       */
+/*   Updated: 2022/12/16 13:22:59 by mkaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include "libft/libft.h"
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include "libft/libft.h"
 
 int		ft_printf(const char *str, ...);
 int		check_print_type(char *str, va_list va, int i);
 int		ft_putstr(char *str);
 int		ft_putnbr(int nb);
 void	ft_putchar(char c);
+
+#endif
